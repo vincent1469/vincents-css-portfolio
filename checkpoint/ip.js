964c7c07a4f964a373a9aa6ac17b4be66835b0eb
Vincent Chu <vchu.sh@mysummitps.org>
2019-02-01 23:30:55 +0000

    Kanto

---
+++ checkpoint/ip.js
@@ -0,0 +1,214 @@
+var sTierDisplay = document.getElementById("sTierDisplay");
+var aTierDisplay = document.getElementById("aTierDisplay");
+var bTierDisplay = document.getElementById("bTierDisplay");
+var cTierDisplay = document.getElementById("cTierDisplay");
+var dTierDisplay = document.getElementById("dTierDisplay");
+
+var blaziken = document.getElementById("blaziken");
+
+blaziken.addEventListener("mousemove", function(){
+    aTierDisplay.innerHTML = "";
+    bTierDisplay.innerHTML = "";
+    dTierDisplay.innerHTML = "";
+    cTierDisplay.innerHTML = "";
+    sTierDisplay.innerHTML = "Blaziken is one of the best starter Pokemon because of his ability, Speed Boost. Speed Boost boosts his Speed stat by one stage after each turn, allowing it to outspeed anyone within a few turns. In addition to that, he has 120 attack and 110 special attack, making it a versatile attacker that excels at both Physical and Special attacks.";
+});
+
+var infernape = document.getElementById("infernape");
+
+infernape.addEventListener("mousemove", function(){
+    aTierDisplay.innerHTML = "";
+    bTierDisplay.innerHTML = "";
+    dTierDisplay.innerHTML = "";
+    cTierDisplay.innerHTML = "";
+    sTierDisplay.innerHTML = "Infernape is another excellent option for a starter because it is a fast Pokemon, with 108 speed. It is also a versatile attacker, with 104 attack and special attack. His ability Iron Fist boosts his moves such as Fire Punch and Mach Punch. Finally, it gets coverage moves such as Thunder Punch and Ice Punch (which are boosted by his ability).";
+});  
+
+var greninja = document.getElementById("greninja");
+
+greninja.addEventListener("mousemove", function(){
+    aTierDisplay.innerHTML = "";
+    bTierDisplay.innerHTML = "";
+    cTierDisplay.innerHTML = "";
+    dTierDisplay.innerHTML = "";
+    sTierDisplay.innerHTML = "Greninja is also great to have. His 128 speed outspeeds almost all Pokemon. He also has 103 special attack to deal powerful attacks. The real reason it is on the S-Tier is his ability, Protean. Protean changes the user's type to the type of the attack that Greninja is using, so all of his attacks will get a 50% damage bonus for having the same type as the user. A high special attack combined with a 50% bonus can deal immense damage to opponents.";
+});
+
+var feraligatr = document.getElementById("feraligatr");
+
+feraligatr.addEventListener("mousemove", function(){
+    sTierDisplay.innerHTML = "";
+    bTierDisplay.innerHTML = "";
+    cTierDisplay.innerHTML = "";
+    dTierDisplay.innerHTML = "";
+    aTierDisplay.innerHTML = "Feraligatr has one of the best offensive abilities in Sheer Force. Sheer Force gives a 30% boost to moves with secondary effects, but removes them. A 30% bonus combined with 105 attack could deal heavy damage to opponents. In addition to that, it gets coverage moves such as Earthquake and Ice Punch. Finally, his Water typing only has two weaknesses, both of which it can counter. Its mediocre 78 speed brings it down to A-Tier, however.";
+});
+
+var decidueye = document.getElementById("decidueye");
+
+decidueye.addEventListener("mousemove", function(){
+    sTierDisplay.innerHTML = "";
+    aTierDisplay.innerHTML = "";
+    cTierDisplay.innerHTML = "";
+    dTierDisplay.innerHTML = "";
+    bTierDisplay.innerHTML = "Decidueye is a great choice for your starter because it has a strong signature move. Its signature move, Spirit Shackle prevents the opponent from switching. This move is great against opponents that use Baton Pass to set up. Finally, it has 107 attack and 100 special attack so that Decidueye can work as a versatile attacker. Although it only has 70 speed, Decidueye can learn priority moves such as Sucker Punch or Shadow Sneak that always go first against non-priority moves. Unfortunately, its Grass/Ghost typing is terrible defensively, with 5 weaknesses.";
+});
+
+var venusaur = document.getElementById("venusaur");
+
+venusaur.addEventListener("mousemove", function(){
+    sTierDisplay.innerHTML = "";
+    bTierDisplay.innerHTML = "";
+    dTierDisplay.innerHTML = "";
+    cTierDisplay.innerHTML = "";
+    aTierDisplay.innerHTML = "Venusaur is one of the more balanced Pokemon among the starters, with no stat falling below 80. Its ability Chlorophyll doubles its speed in the sun, allowing Venusaur to counter Fire-types with Sunny Day. It also gets a reliable coverage move in Earthquake. However, its Grass/Poison typing has weaknesses to Fire, Flying, Psychic, and Ice, all of which are strong offensive types that can easily kill Venusaur.";
+});
+
+var swampert = document.getElementById("swampert");
+swampert.addEventListener("mousemove", function(){
+    sTierDisplay.innerHTML = "";
+    bTierDisplay.innerHTML = "";
+    dTierDisplay.innerHTML = "";
+    cTierDisplay.innerHTML = "";
+    aTierDisplay.innerHTML = "Swampert is a strong option for a starter because it has one of the best typings in Water/Ground. His only weakness is a 4x weakness Grass. However, he can counter this with coverage moves like Ice Beam that can be learned through TMs. His stats are also balanced, with 110 attack and 90 defense. The only thing keeping it from S-Tier is his abysmal 60 speed, and he doesn't get Swift Swim like his Mega Evolution does.";
+});
+
+var serperior = document.getElementById("serperior");
+
+serperior.addEventListener("mousemove", function(){
+    sTierDisplay.innerHTML = "";
+    bTierDisplay.innerHTML = "";
+    dTierDisplay.innerHTML = "";
+    cTierDisplay.innerHTML = "";
+    aTierDisplay.innerHTML = "Serperior is a solid Pokemon that is highlighted by its ability, Contrary. Contrary reverses all stat changes. For example, Serperior's move Leaf Storm normally lowers Serperior's Special Attack by 2 stages, but with Contrary, it is increased by 2 stages! In addition to that, it has 113 speed to outspeed Fire-types such as Infernape or Charziard that the opponent might switch into. However, Serperior has a very limited movepool, only being able to learn Grass, Dark, Dragon, and Flying moves. Only one of Serperior's five weaknesses can be countered with Serperior's moveset.";
+});
+
+var primarina = document.getElementById("primarina");
+
+primarina.addEventListener("mousemove", function(){
+    sTierDisplay.innerHTML = "";
+    dTierDisplay.innerHTML = "";
+    bTierDisplay.innerHTML = "";
+    cTierDisplay.innerHTML = "";
+    aTierDisplay.innerHTML = "Primarina has 126 special attack and 116 special defense, making it a powerful all-around Pokemon. It's ability Liquid Voice makes sound moves water type, and combined with the move Hyper Voice gives Primarina a 50% damage boost. Like Swampert, she only has 60 speed, preventing her from being S-Tier.";
+});
+
+var sceptile = document.getElementById("sceptile");
+
+sceptile.addEventListener("mousemove", function(){
+    sTierDisplay.innerHTML = "";
+    dTierDisplay.innerHTML = "";
+    aTierDisplay.innerHTML = "";
+    bTierDisplay.innerHTML = "";
+    cTierDisplay.innerHTML = "Sceptile is another fast Pokemon (120 speed) that can hit hard (105 special attack). Sceptile also has the ability Unburden, which doubles its speed after using its item. Similar to Serperior, Sceptile's downfall is its bad typing, with Grass having 5 weaknesses. However, Sceptile is extremely frail with only 65 defense.";
+});
+
+var incineroar = document.getElementById("incineroar");
+
+incineroar.addEventListener("mousemove", function(){
+    sTierDisplay.innerHTML = "";
+    aTierDisplay.innerHTML = "";
+    bTierDisplay.innerHTML = "";
+    dTierDisplay.innerHTML = "";
+    cTierDisplay.innerHTML = "Incineroar is a powerful physical attacker, with 115 attack. In addition to that, its defensive stats are well-rounded with 90 for defense and special defense. Also, its ability Intimidate, which lowers the stat stage of all opponents by 1 upon entering battle, is great against strong physical attackers. However, its speed of 60 hinders Incineroar greatly. Unlike Decidueye, it does not have priority moves to make up for this.";
+});
+
+var charizard = document.getElementById("charizard");
+
+charizard.addEventListener("mousemove", function(){
+    sTierDisplay.innerHTML = "";
+    aTierDisplay.innerHTML = "";
+    bTierDisplay.innerHTML = "";
+    dTierDisplay.innerHTML = "";
+    cTierDisplay.innerHTML = "Charizard is a powerful special attacker with 109 special attack, and it has 100 speed to back it up. It also gets coverage moves in Earthquake and Focus Blast. However, his typing of Fire/Flying has a 4x weakness to Rock. Stealth Rocks (entry hazards that do damage based on Rock-type effectiveness) halve his health upon entering, making it easy to kill. Also, his defensive stats are subpar, with only 78 health and defense.";
+});
+
+var emboar = document.getElementById("emboar");
+
+emboar.addEventListener("mousemove", function(){
+    sTierDisplay.innerHTML = "";
+    aTierDisplay.innerHTML = "";
+    bTierDisplay.innerHTML = "";
+    cTierDisplay.innerHTML = "";
+    dTierDisplay.innerHTML = "Emboar is a D-Tier Pokemon, but has some highlights. First off, lets start with the positives. It has 123 attack, 110 health, and 100 special attack. That is all poor Emboar has going for it, unfortunately. His defense, special defense, and speed stats are all at 65, making it a sitting duck for opponents. In addition to that, many of his useful moves have a negative effect associated to them. Flare Blitz and Wild Charge both have recoil, and Superpower lowers Emboar's attack and defense. In addition to that, his ability does not help this issue, unlike abilities like Rock Head (removes recoil) or Magic Guard (removes indirect damage). Instead, his ability is Reckless, which deals 20% more damage with moves that have recoil. While the effect is good, it causes Emboar to take more recoil damage. Emboar can only survive a few hits before dying, and does not have a reliable speed to get off multiple attacks.";
+});
+
+var chesnaught = document.getElementById("chesnaught");
+
+chesnaught.addEventListener("mousemove", function(){
+    sTierDisplay.innerHTML = "";
+    aTierDisplay.innerHTML = "";
+    dTierDisplay.innerHTML = "";
+    cTierDisplay.innerHTML = "";
+    bTierDisplay.innerHTML = "Chesnaught is incredibly bulky, with 122 defense. Its ability Bulletproof can be used situationally. In addition to that, Chesnaught has 107 attack for it to use powerful physical moves such as Wood Hammer and Drain Punch. Also, its signature move, Spiky Shield, does damage if the opponent makes contact with it. Unfortunately for Chesnaught, hiis Grass/Fighting type has 6 weaknesses, and his 75 special defense and his 64 speed doesn't help.";
+});
+
+var meganium = document.getElementById("meganium");
+
+meganium.addEventListener("mousemove", function(){
+    sTierDisplay.innerHTML = "";
+    aTierDisplay.innerHTML = "";
+    bTierDisplay.innerHTML = "";
+    dTierDisplay.innerHTML = "";
+    cTierDisplay.innerHTML = "Meganium has decent bulk, with 100 defense and special defense. It has 80 health and speed, 82 attack, and 83 special attack. Not terrible stats, but not good enough to top other grass starters such as Decidueye or Venusaur. Meganium suffers the same fate many Grass-types do, having 5 weaknesses. Although its moveset is slightly better than Serperior's, it still cannot counter a majority of its weaknesses.";
+});
+
+var torterra = document.getElementById("torterra");
+
+torterra.addEventListener("mousemove", function(){
+    sTierDisplay.innerHTML = "";
+    aTierDisplay.innerHTML = "";
+    bTierDisplay.innerHTML = "";
+    cTierDisplay.innerHTML = "";
+    dTierDisplay.innerHTML = "Torterra is a D-Tier Pokemon, but at least it has a few redeeming qualities. Torterra has 100 defense and 85 special defense, plus 95 health for some more bulk. Also, it has 109 attack and access to a lot of coverage moves. Unfortunately, Torterra has only 56 speed. It has the least speed of all starter Pokemon and any C-Tier Pokemon can outspeed and kill it quickly. In addition to that, its Grass/Ground typing has 4 weaknesses, including a 4x weakness to Ice. Ice-types are among the strongest offensive Pokemon, and to add insult to injury, many Pokemon can learn Ice Beam through TMs. At least his ability Shell Armor, which prevents critical hits against Torterra, helps with Torterra's bulk. ";
+});
+
+var blastoise = document.getElementById("blastoise");
+
+blastoise.addEventListener("mousemove", function(){
+    sTierDisplay.innerHTML = "";
+    aTierDisplay.innerHTML = "";
+    bTierDisplay.innerHTML = "";
+    dTierDisplay.innerHTML = "";
+    cTierDisplay.innerHTML = "Blastoise is a good tank, with 100 defense and 105 special defense. It can clear stage hazards with Rapid Spin and it has coverage moves such as Ice Beam. However, its offensive capabilities are limited with 83 attack and 85 special attack. Also, it has a mediocre 78 speed.";
+});
+
+var empoleon = document.getElementById("empoleon");
+
+empoleon.addEventListener("mousemove", function(){
+    sTierDisplay.innerHTML = "";
+    aTierDisplay.innerHTML = "";
+    cTierDisplay.innerHTML = "";
+    dTierDisplay.innerHTML = "";
+    bTierDisplay.innerHTML = "Empoleon is a great support Pokemon since it has access to a wide variety of status moves. Defog can clear stage hazards, Stealth Rock is a reliable stage hazard to set, and Roar can switch out buffed opponents. Also, it has 88 defense and 101 special defense to stall for longer to allow Empoleon to use more status moves. Unfortunately, Empoleon's speed of 60 isn't good competitively, and status users especially need.";
+});
+
+var typhlosion = document.getElementById("typhlosion");
+
+typhlosion.addEventListener("mousemove", function(){
+    sTierDisplay.innerHTML = "";
+    aTierDisplay.innerHTML = "";
+    cTierDisplay.innerHTML = "";
+    dTierDisplay.innerHTML = "";
+    bTierDisplay.innerHTML = "Typhlosion is a fast special attacker, with 109 special attack and 100 attack. In addition to that, Typhlosion can access Eruption, which does more damage the more health you have. If Typhlosion goes first, it can get off a powerful Eruption attack to start off. However, Typhlosion is easily countered by many types, and it doesn't help that its defense is 78 and its special defense is 85.";
+});
+
+var delphox = document.getElementById("delphox");
+
+delphox.addEventListener("mousemove", function(){
+    sTierDisplay.innerHTML = "";
+    aTierDisplay.innerHTML = "";
+    cTierDisplay.innerHTML = "";
+    dTierDisplay.innerHTML = "";
+    bTierDisplay.innerHTML = "Delphox is a fast (104 speed) special attacker (114 special attack) that has some bulk (100 special defense). Its signature move, Mystical Fire, lowers the opponent's Special Attack by one stage while also doing damage. Its Fire/Psychic typing is great offensively, but terrible defensively. Delphox has five weaknesses, and its 72 defense doesn't help.";
+});
+
+var samurott = document.getElementById("samurott");
+
+samurott.addEventListener("mousemove", function(){
+    sTierDisplay.innerHTML = "";
+    aTierDisplay.innerHTML = "";
+    bTierDisplay.innerHTML = "";
+    dTierDisplay.innerHTML = "";
+    cTierDisplay.innerHTML = "Samurott is a versatile attacker, with 100 attack and 107 special attack. Its pure Water typing only gives it two weaknesses (Electric and Grass), both of which he can counter with moves such as Dig or Ice Beam. Unfortunately, Samurott's other stats are not great. His special defense of 70 is especially bad against his weaknesses, both of which use special attacks. In addition to that, his 70 speed makes him an easy for opponents.";
+});
